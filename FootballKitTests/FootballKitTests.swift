6beb64fd6b222e4b9f0e1fe9359e0dcfee1e00c0
Bilal Bas <bilal.bas@gmx.de>
2025-02-18 19:31:35 +0100

    Initial Commit

---
+++ FootballKitTests/FootballKitTests.swift
@@ -0,0 +1,17 @@
+//
+//  FootballKitTests.swift
+//  FootballKitTests
+//
+//  Created by Bilal Baş on 18.02.25.
+//
+
+import Testing
+@testable import FootballKit
+
+struct FootballKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
